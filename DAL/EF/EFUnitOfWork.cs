156147a1ab52ed4e02b6a4b0997da883f46bda99
Vlad <stukalin.vlad@gmail.com>
2024-12-17 22:19:18 +0200

    Added realization for unit of work

---
+++ DAL/EF/EFUnitOfWork.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Films.DAL.Repositories.Implementations;
+using Films.DAL.UnitOfWork;
+using Films.DAL.Repositories.Interfaces;
+
+namespace Films.DAL.EF
+{
+    public class EFUnitOfWork
+        : IUnitOfWork
+    {
+        private FilmContext db;
+        private FilmRepository filmRepository;
+        private AdminRepository adminRepository;
+        public EFUnitOfWork(DbContextOptions options)
+        {
+            db = new FilmContext(options);
+        }
+        public IFilmRepository Films
+        {
+            get
+            {
+                if (filmRepository == null)
+                    filmRepository = new FilmRepository(db);
+                return filmRepository;
+            }
+        }
+        public IAdminRepository Admins
+        {
+            get
+            {
+                if (adminRepository == null)
+                    adminRepository = new AdminRepository(db);
+                return adminRepository;
+            }
+        }
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+        private bool disposed = false;
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+    }
+}
