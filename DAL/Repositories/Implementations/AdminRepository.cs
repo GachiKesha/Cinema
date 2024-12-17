de6cbccb90d37a0b0862142766739f8dd064e15f
Vlad <stukalin.vlad@gmail.com>
2024-12-17 22:18:31 +0200

    Added Repositories

---
+++ DAL/Repositories/Implementations/AdminRepository.cs
@@ -0,0 +1,16 @@
+﻿using Films.DAL.EF;
+using Films.DAL.Entities;
+using Films.DAL.Repositories.Interfaces;
+
+namespace Films.DAL.Repositories.Implementations
+{
+    public class AdminRepository
+    : BaseRepository<Admin>, IAdminRepository
+    {
+        internal AdminRepository(FilmContext context)
+        : base(context)
+        {
+        }
+    }
+
+}
---
+++ DAL/Repositories/Implementations/FilmRepository.cs
@@ -0,0 +1,15 @@
+﻿using Films.DAL.EF;
+using Films.DAL.Entities;
+using Films.DAL.Repositories.Interfaces;
+
+namespace Films.DAL.Repositories.Implementations
+{
+    public class FilmRepository
+        : BaseRepository<Film>, IFilmRepository
+    {
+        internal FilmRepository(FilmContext context)
+        : base(context)
+        {
+        }
+    }
+}
