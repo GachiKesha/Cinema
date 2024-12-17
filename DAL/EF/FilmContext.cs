d9312fc54da4c3abd3cde8dcc0c14d1ec7072f4d
Vlad <stukalin.vlad@gmail.com>
2024-12-17 22:12:45 +0200

    Added FilmContext

---
+++ DAL/EF/FilmContext.cs
@@ -0,0 +1,16 @@
+﻿using Films.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Films.DAL.EF
+{
+    public class FilmContext
+        : DbContext
+    {
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public FilmContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+    }
+}
