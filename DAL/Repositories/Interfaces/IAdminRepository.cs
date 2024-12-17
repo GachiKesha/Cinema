70e7d90d25936552245172412f0ffc50c4ac6089
Vlad <stukalin.vlad@gmail.com>
2024-12-17 17:07:47 +0200

    Added Contracts of Repositories

---
+++ DAL/Repositories/Interfaces/IAdminRepository.cs
@@ -0,0 +1,9 @@
+﻿using Films.DAL.Entities;
+
+namespace Films.DAL.Repositories.Interfaces
+{
+    public interface IAdminRepository
+        : IRepository<Admin>
+    {
+    }
+}
---
+++ DAL/Repositories/Interfaces/IFilmRepository.cs
@@ -0,0 +1,9 @@
+﻿using Films.DAL.Entities;
+
+namespace Films.DAL.Repositories.Interfaces
+{
+    public interface IFilmRepository
+        : IRepository<Film>
+    {
+    }
+}
