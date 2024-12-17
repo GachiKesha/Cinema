1d4702c64002a1c4a270a2639f480c5620b6aac4
Vlad <stukalin.vlad@gmail.com>
2024-12-17 23:45:16 +0200

    Added test for admin rep

---
+++ DAL.Tests/TestAdminRepository.cs
@@ -0,0 +1,20 @@
+﻿using Films.DAL.Entities;
+using Films.DAL.Repositories.Implementations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Films.DAL.Tests
+{
+    class TestAdminRepository
+        : BaseRepository<Admin>
+    {
+        public TestAdminRepository(DbContext context)
+            : base(context)
+        {
+        }
+    }
+}
