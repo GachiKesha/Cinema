edf21226c2b5aae4f87462612790d6e6015bb959
Vlad <stukalin.vlad@gmail.com>
2024-12-17 22:18:58 +0200

    Added interface for unit of work

---
+++ DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using Films.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Films.DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IFilmRepository Films { get; }
+        IAdminRepository Admins { get; }
+        void Save();
+    }
+}
